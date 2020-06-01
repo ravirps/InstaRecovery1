d8ae614dd3df7be0e0feb32c7f8a1393776aa067
Ravi Pratap Singh <raviand26748@gmail.com>
2020-06-01 23:01:44 +0530

    made message button

--- app/src/main/java/com/example/instarecovery/MainActivity.java
+++ app/src/main/java/com/example/instarecovery/MainActivity.java
@@ -12,6 +12,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.DocumentsContract;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +24,7 @@ import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
+    public static String messagedata;
     private static final int PERMISSION_REQUEST_CODE = 100;
     private static final int PICK_PDF_FILE = 2;
     private String readTextFromUri(Uri uri) throws IOException {
@@ -38,39 +40,39 @@ public class MainActivity extends AppCompatActivity {
         }
         return stringBuilder.toString();
     }
+
     private void openFile( ) {
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
-        intent.setType("*/*");
-
-
+        intent.setType("application/json");
         startActivityForResult(intent, PICK_PDF_FILE);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView=findViewById(R.id.text);
-         openFile();
+        textView=findViewById(R.id.messages);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFile();
+                textView.setText(messagedata);
+            }
+        });
+
 
     }
     @Override
     public void onActivityResult(int requestCode, int resultCode,
                                  Intent resultData) {
         super.onActivityResult(requestCode, resultCode, resultData);
-        if (requestCode == PICK_PDF_FILE
-                && resultCode == Activity.RESULT_OK) {
-            // The result data contains a URI for the document or directory that
-            // the user selected.
+        if (requestCode == PICK_PDF_FILE  && resultCode == Activity.RESULT_OK) {
             Uri uri = null;
             if (resultData != null) {
                 uri = resultData.getData();
+                Log.i("logcat",uri.getPath());
                 try {
-                    String text=readTextFromUri(uri);
-                    if(text!=null)
-                    {
-                        textView.setText(text);
-                    }
+                    messagedata=readTextFromUri(uri);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -80,35 +82,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private boolean checkPermission() {
-        int result = ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.READ_EXTERNAL_STORAGE);
-        if (result == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 
-    private void requestPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
-            Toast.makeText(MainActivity.this, "Write External Storage permission allows us to save files. Please allow this permission in App Settings.", Toast.LENGTH_LONG).show();
-        } else {
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSION_REQUEST_CODE:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.e("value", "Permission Granted, Now you can use local drive .");
-                    openFile();
-                } else {
-                    Log.e("value", "Permission Denied, You cannot use local drive .");
-                }
-                break;
-        }
-    }
 }
 
---
+++ app/src/main/java/com/example/instarecovery/messages/chats.java
@@ -0,0 +1,16 @@
+package com.example.instarecovery.messages;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.instarecovery.R;
+
+public class chats extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chats);
+    }
+}
